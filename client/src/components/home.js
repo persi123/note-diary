0f36d60a9c986607aeacc5cfb52f73489ba51dae
persi123 <tongrapersi1@gmail.com>
2020-07-30 23:17:28 +0530

    "config"

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -41,7 +41,7 @@ export default function Home() {
     useEffect(() => {
     if(id){
         let url=process.env.NODE_ENV === "production"? process.env.BASE_URL:"http://localhost:3000";
-     TinyUrl.shorten(`/note/${id}`, function(res, err) {
+     TinyUrl.shorten(`http://localhost:3000/note/${id}`, function(res, err) {
         if (err)
             console.log(err)
         setlink(res); //Returns a shorter version of http://google.com - http://tinyurl.com/2tx
